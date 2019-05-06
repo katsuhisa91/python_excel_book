2bcb59613bddbf74b02350978fd8f4db97299251
katsuhisa91 <91katsuhisa@gmail.com>
2019-05-06 13:14:02 +0900

    add sample program about list in

---
+++ sample/list4.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'orange', 'banana']
+
+if 'apple' in fruits:
+    print('appleあります！！！')
+else:
+    print('appleありません！！！')
+
+if 'grape' in fruits:
+    print('grapeあります！！！')
+else:
+    print('grapeありません！！！')
