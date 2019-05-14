c35c42c80edbbb7fd8e99e00144165a517bbb641
katsuhisa91 <91katsuhisa@gmail.com>
2019-05-14 17:13:26 +0900

    add sample program 4 of "for loop"

---
+++ sample/for_loop4.py
@@ -0,0 +1,10 @@
+fruits = {'apple': 100, 'orange': 200}
+
+for fruit in fruits.keys():
+    print(fruit)
+
+for money in fruits.values():
+    print(money)
+
+for fruit, money in fruits.items():
+    print(fruit, money)
