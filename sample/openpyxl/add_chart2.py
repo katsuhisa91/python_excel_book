76eec8daf95ad31acc2f68d513ead3c00c80dcd8
katsuhisa91 <91katsuhisa@gmail.com>
2019-09-24 16:55:00 +0900

    グラフを作成するサンプルプログラムを書籍の解説に沿って修正

--- sample/openpyxl/add_chart2.py
+++ sample/openpyxl/add_chart2.py
@@ -1,5 +1,5 @@
 import openpyxl
-from openpyxl.chart import BarChart, Reference
+from openpyxl.chart import BarChart, Reference, Series
 
 wb = openpyxl.Workbook()
 ws = wb.active
@@ -7,10 +7,11 @@ for i in range(10):
     ws.append([i])
 
 ref_obj = Reference(ws, min_col=1, min_row=1, max_col=1, max_row=10)
+series_obj = Series(ref_obj, title='sample series')
 
 chart = BarChart()
 chart.title = 'sample chart'
-chart.add_data(ref_obj)
+chart.append(series_obj)
 
 ws.add_chart(chart, 'C1')
-wb.save('sample_chart.xlsx')
+wb.save('sample_chart2.xlsx')
