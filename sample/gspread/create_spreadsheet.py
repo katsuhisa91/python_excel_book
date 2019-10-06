c885b779c10a56b46306c0eb8d2cd83a799ddf63
katsuhisa91 <91katsuhisa@gmail.com>
2019-10-06 15:48:39 +0900

    ファイル名を処理の実態に沿って修正

---
+++ sample/gspread/create_spreadsheet.py
@@ -0,0 +1,15 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+# Google Cloud PlatformからダウンロードしたJSONファイルを指定
+key_name = 'gspread-xxxxxxxxxxxx.json'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(key_name, scope)
+gc = gspread.authorize(credentials)
+
+sh = gc.create('A new spreadsheet')
+sh.share('91katsuhisa@gmail.com', perm_type='user', role='writer')
+print('https://docs.google.com/spreadsheets/d/' + sh.id)
