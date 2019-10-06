3e97a38e01b45145664d4867fc9920db9fa5be2d
katsuhisa91 <91katsuhisa@gmail.com>
2019-10-06 15:59:36 +0900

    ワークシートを追加するサンプルプログラムを追加

--- sample/gspread/add_worksheet.py
+++ sample/gspread/add_worksheet.py
@@ -10,6 +10,11 @@ key_name = 'gspread-xxxxxxxxxxxx.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(key_name, scope)
 gc = gspread.authorize(credentials)
 
-sh = gc.create('A new spreadsheet')
-sh.share('91katsuhisa@gmail.com', perm_type='user', role='writer')
-print('https://docs.google.com/spreadsheets/d/' + sh.id)
+sp = gc.create('A new spreadsheet')
+
+# 新しく2行4列のワークシートを追加
+sp.add_worksheet('A new worksheet', 2, 4)
+print(sp.worksheets())
+
+sp.share('91katsuhisa@gmail.com', perm_type='user', role='writer')
+print('https://docs.google.com/spreadsheets/d/' + sp.id)
