028b6961a4996f543083912f0e913044dc2d3498
katsuhisa91 <91katsuhisa@gmail.com>
2019-10-06 15:32:11 +0900

    環境固有のIDを指定しないように変更

--- sample/gspread/get_column.py
+++ sample/gspread/get_column.py
@@ -5,9 +5,9 @@ scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
 # スプレッドシートのIDを指定
-sp_id = '1iONW2lt23IM74J7A2RysUVes-06kGxJbgZlz7sTeZlY'
+sp_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 # Google Cloud PlatformからダウンロードしたJSONファイルを指定
-key_name = 'gspread-948be8b5928d.json'
+key_name = 'gspread-xxxxxxxxxxxx.json'
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(key_name, scope)
 gc = gspread.authorize(credentials)
@@ -15,11 +15,10 @@ gc = gspread.authorize(credentials)
 sp = gc.open_by_key(sp_id)
 worksheet = sp.worksheet('シート1')
 
-column = worksheet.col_values(2)
-for cell in column:
-    print(cell)
-
 # 別の取り出し方法
 # column = worksheet.range('B1:B5')
 # for cell in column:
 #     print(cell.value)
+column = worksheet.col_values(2)
+for cell in column:
+    print(cell)
