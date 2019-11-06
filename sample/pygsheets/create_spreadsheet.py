498480ba23063ddd79ca2449328ace51fa8722a7
katsuhisa91 <91katsuhisa@gmail.com>
2019-11-06 20:17:57 +0900

    pygsheetsでスプレッドシートを作成するサンプルプログラムを追加

---
+++ sample/pygsheets/create_spreadsheet.py
@@ -0,0 +1,11 @@
+import pygsheets
+
+# Google Cloud PlatformからダウンロードしたJSONファイルを指定
+gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
+sp = gc.create('A new spreadsheet')
+
+# example@gmail.comの箇所は、メールアドレスを置き換えてください
+# roleの種別は、下記ドキュメントを参照
+# https://developers.google.com/drive/api/v3/reference/permissions#resource
+sp.share('example@gmail.com', role='writer')
+print('https://docs.google.com/spreadsheets/d/' + sp.id)
