0775d7f296133641b629ce01ca81d9ec1aa9bb43
katsuhisa91 <91katsuhisa@gmail.com>
2019-11-07 14:53:36 +0900

    不要なコメントを削除

--- sample/pygsheets/get_cell.py
+++ sample/pygsheets/get_cell.py
@@ -1,10 +1,6 @@
 import pygsheets
 
 gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
-
-# スプレッドシートのIDを指定
-# 書籍で利用するサンプルスプレッドシートのIDは以下
-# '1iONW2lt23IM74J7A2RysUVes-06kGxJbgZlz7sTeZlY'
 sp = gc.open_by_key('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 # wks = sp.worksheet_by_title("シート1") でもOK
