b994a8a044e210dd813bc0ef975731aa92b32b40
katsuhisa91 <91katsuhisa@gmail.com>
2019-11-11 22:31:08 +0900

    サービスアカウントへの権限付与は、スプレッドシート側で操作する

--- sample/pygsheets/get_row.py
+++ sample/pygsheets/get_row.py
@@ -3,10 +3,6 @@ import pygsheets
 gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
 sp = gc.open_by_key('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
-# 操作実行前に、対象スプレッドシートで、権限の共有をする
-# JSONファイルのclient_emailのメールアドレスで置き換えする
-sp.share('pygsheets-test@pygsheets-xxxxxx.iam.gserviceaccount.com', role='reader')
-
 # wks = sp.worksheet_by_title("シート1") でもOK
 wks = sp.worksheet('index', 0)
 # 2行目の値を取り出す
--- sample/pygsheets/update_cell.py
+++ sample/pygsheets/update_cell.py
@@ -4,10 +4,6 @@ gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
 sp = gc.open_by_key('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 wks = sp.worksheet('index', 0)
-
-# 編集操作実行前に、対象スプレッドシートで、編集権限の共有する
-# JSONファイルのclient_emailのメールアドレスで置き換えする
-sp.share('pygsheets-test@pygsheets-xxxxxx.iam.gserviceaccount.com', role='writer')
 wks.update_value('C1', '100')
 
 sp.share('example@gmail.com', role='reader')
