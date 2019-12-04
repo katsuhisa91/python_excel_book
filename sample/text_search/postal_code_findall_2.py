d9247fcda03e295ee871942a7e5d99daa2ada0ab
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-05 01:40:42 +0900

    正規表現のパターンを追加

--- sample/text_search/postal_code_findall_2.py
+++ sample/text_search/postal_code_findall_2.py
@@ -1,6 +1,8 @@
 import re
 
 postal_code_regex = re.compile(r'\d\d\d-\d\d\d\d')
+# こちらと同義
+# postal_code_regex = re.compile(r'\d{3}-\d{4}')
 
 with open('./file.txt') as f:
     postal_code_list = postal_code_regex.findall(f.read())
