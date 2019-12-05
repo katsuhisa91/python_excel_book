b0fd4f619ed75dceb5ad2bc74d3f9f5368f9ce62
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-05 15:27:23 +0900

    文字列置換のサンプルを追加

---
+++ sample/text_search/sub.py
@@ -0,0 +1,7 @@
+import re
+
+with open('./file.txt') as f:
+    text_mod = re.sub('東京', 'Tokyo', f.read())
+    # 以下のように正規表現も使えます
+    # text_mod = re.sub('.京', 'Tokyo', f.read())
+    print(text_mod)
