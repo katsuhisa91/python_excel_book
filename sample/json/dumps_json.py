409deea5d4a080424c9857e5dcaf13a762c0ae5c
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-08 22:52:35 +0900

    JSONファイルをダンプするプログラムを追加

---
+++ sample/json/dumps_json.py
@@ -0,0 +1,10 @@
+import json
+
+task_data = {
+    'id': 1,
+    'name': 'Homework',
+    'deadline': '2020/09/01'
+}
+
+with open('sample.json', 'w', newline='') as f:
+    f.write(json.dumps(task_data))
