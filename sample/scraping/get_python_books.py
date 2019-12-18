21ea7f8c0168524efb0a68bede01aec5f40192d1
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-19 01:20:19 +0900

    パラメータの取り扱い方法を修正

--- sample/scraping/get_python_books.py
+++ sample/scraping/get_python_books.py
@@ -3,17 +3,21 @@ from bs4 import BeautifulSoup
 import time
 
 for page_num in range(4):
-    url = 'xxxxxx&page={}'.format(page_num)
-    r = requests.get(url)
+    url = 'xxxxx'
+    params = {
+        'g': 'プログラミング・システム開発',
+        'page': page_num
+    }
+    r = requests.get(url, params)
     html = r.text
     soup = BeautifulSoup(html, 'lxml')
-    book_list = soup.select('div.data > h3 > a')
+    book_elems = soup.select('#mainbook > ul > li > div.data > h3 > a')
 
-    for book in book_list:
-        book_title = book.text
+    for book_elem in book_elems:
+        book_title = book_elem.text
         if 'Python' in book_title:
             print(book_title)
-            book_url = book.get('href')
-            print('https://gihyo.jp' + book_url)
+            book_url = book_elem.get('href')
+            print('xxxxx' + book_url)
 
     time.sleep(1)
