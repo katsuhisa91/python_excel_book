c86b4edfd4a0d364c9929b48f32ac9c3cbc50fc5
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-20 20:53:13 +0900

    ヘッドレス環境で動作させる際の方法をコメントで記載しておく

--- sample/scraping/check_search_ranking.py
+++ sample/scraping/check_search_ranking.py
@@ -1,6 +1,11 @@
 import time
 from selenium import webdriver
 
+# headless環境で動かす際の設定
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+# driver = webdriver.Chrome('/Applications/chromedriver', options=options)
+
 # 必要に応じ、chromedriverのパスを書き換えて下さい
 driver = webdriver.Chrome('/Applications/chromedriver')
 
@@ -13,7 +18,6 @@ time.sleep(1)
 search_box.submit()
 time.sleep(1)
 results = driver.find_elements_by_css_selector('div.r h3')
-driver.save_screenshot('screenshot.png')
 
 for index, result in enumerate(results):
     print('検索順位{}位のサイトは: '.format(index + 1) + result.text)
