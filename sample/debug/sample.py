745c2e5125f601efca3892f196ee790d468fd2e4
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-28 17:22:26 +0900

    例外処理を入れる前の、エラーをすぐ発生させることができるサンプルコードを追加

--- sample/debug/sample.py
+++ sample/debug/sample.py
@@ -1,11 +1,4 @@
 sample_list = [1, 2, 3]
 
-try:
-    input_num = int(input('取り出したい値を教えて下さい: '))
-except ValueError as e:
-    print('数字を入力してください')
-
-try:
-    print(sample_list[input_num])
-except IndexError as e:
-    print('リストには値が3つしか入っていないので、0〜2の値を指定してください')
+input_num = int(input('取り出したい値を教えて下さい: '))
+print(sample_list[input_num])
