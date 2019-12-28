eca88311522999b642380fd7654dc6855c0cd0c0
katsuhisa91 <91katsuhisa@gmail.com>
2019-12-28 18:29:36 +0900

    エラーの表示方法を変更

--- sample/try_exception/sample_try_exception.py
+++ sample/try_exception/sample_try_exception.py
@@ -3,9 +3,9 @@ sample_list = [1, 2, 3]
 try:
     input_num = int(input('取り出したい値を教えて下さい: '))
 except ValueError as e:
-    print('数字を入力してください')
+    print('catch ValueError:', e)
 
 try:
     print(sample_list[input_num])
 except IndexError as e:
-    print('リストには値が3つしか入っていないので、0〜2の値を指定してください')
+    print('catch IndexError:', e)
