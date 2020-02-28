701db954f41d8c27e9b0502660545c8ea6c40ceb
katsuhisa91 <91katsuhisa@gmail.com>
2020-02-28 10:01:47 +0900

    コメントを修正

--- sample/openpyxl/set_freeze_panes.py
+++ sample/openpyxl/set_freeze_panes.py
@@ -3,7 +3,7 @@ import openpyxl
 wb = openpyxl.Workbook()
 ws = wb.active
 
-# A1〜Y49のセルに、セル名を入力する
+# A1〜Y49のセルに、セル名を入力する処理
 for row_num in range(1, 50):
     for column_num in range(1, 26):
         ws.cell(row=row_num, column=column_num).value = chr(ord('A') + column_num - 1) + str(row_num)
