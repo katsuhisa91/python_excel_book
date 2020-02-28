869f8bf9dc90d280d6902a3fad43210444f3db7d
katsuhisa91 <91katsuhisa@gmail.com>
2020-02-28 09:55:57 +0900

    行列固定がわかりやすくなるように、プログラムを修正

--- sample/openpyxl/set_freeze_panes.py
+++ sample/openpyxl/set_freeze_panes.py
@@ -1,6 +1,12 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('shopping.xlsx')
+wb = openpyxl.Workbook()
 ws = wb.active
+
+# A1〜Y49のセルに、セル名を入力する
+for row_num in range(1, 50):
+    for column_num in range(1, 26):
+        ws.cell(row=row_num, column=column_num).value = chr(ord('A') + column_num - 1) + str(row_num)
+
 ws.freeze_panes = 'A2'
 wb.save('test.xlsx')
