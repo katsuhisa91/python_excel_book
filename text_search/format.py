b3140724554c01814fefc9a5d084f30b03eaa0a5
Katsuhisa Kitano <91katsuhisa@gmail.com>
2020-06-15 21:52:44 +0900

    新しいサンプルプログラムを追加

---
+++ text_search/format.py
@@ -0,0 +1,4 @@
+created_date = '202001'
+
+invoice_name = '請求書_{}'.format(created_date)
+print(invoice_name)
---
+++ text_search/format2.py
@@ -0,0 +1,5 @@
+created_year = '2020'
+created_month = '02'
+
+invoice_name = '請求書_{0}年{1}月'.format(created_year, created_month)
+print(invoice_name)
