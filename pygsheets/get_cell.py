27773c8c3830dbd310eb24c0cce80a7d6b777930
Katsuhisa Kitano <91katsuhisa@gmail.com>
2020-07-12 16:40:45 +0900

    書籍にあわせてプログラムを更新する

--- pygsheets/get_cell.py
+++ pygsheets/get_cell.py
@@ -1,12 +1,8 @@
 import pygsheets
 
 gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
-sp = gc.open_by_key('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+sp = gc.open_by_key('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
-# wks = sp.worksheet('id', 0) でも,
-# wks = sp.worksheet('title', 'シート1') でも,
-# wks = sp.worksheet_by_title('シート1') でもOK
 wks = sp.worksheet('index', 0)
-# value = wks.cell((1,2)).value でもOK
 value = wks.cell('B1').value
 print(value)
