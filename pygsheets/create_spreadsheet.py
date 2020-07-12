0d8e431d64a700ec864a5864d03f1789fd89abf7
Katsuhisa Kitano <91katsuhisa@gmail.com>
2020-07-12 16:38:49 +0900

    書籍にあわせてプログラムを更新する

--- pygsheets/create_spreadsheet.py
+++ pygsheets/create_spreadsheet.py
@@ -3,9 +3,7 @@ import pygsheets
 # Google Cloud PlatformからダウンロードしたJSONファイルを指定
 gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
 sp = gc.create('A new spreadsheet')
-
-# example@gmail.comの箇所は、ご自身のメールアドレスに置き換えてください
-# roleの種別は、下記ドキュメントを参照
-# https://developers.google.com/drive/api/v3/reference/permissions#resource
+# 編集権限を指定
 sp.share('example@gmail.com', role='writer')
+
 print('https://docs.google.com/spreadsheets/d/' + sp.id)
