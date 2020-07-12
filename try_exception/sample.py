cc9359215444b92748bf0c7ec47ee0e78c2e2530
Katsuhisa Kitano <91katsuhisa@gmail.com>
2020-07-12 15:56:02 +0900

    書籍にあわせてプログラムを削除

--- try_exception/sample.py
+++
@@ -1,4 +0,0 @@
-sample_list = [1, 2, 3]
-input_value = input('取り出したい値を教えて下さい: ')
-
-print(sample_list[int(input_value)])
--- try_exception/sample_try_exception.py
+++
@@ -1,9 +0,0 @@
-sample_list = [1, 2, 3]
-input_value = input('取り出したい値を教えて下さい: ')
-
-try:
-    print(sample_list[int(input_value)])
-except IndexError as e:
-    print('catch IndexError:', e)
-except ValueError as e:
-    print('catch ValueError:', e)
