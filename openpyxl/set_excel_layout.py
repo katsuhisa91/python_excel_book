5aac3caebd7766d4ff20cedfb5a9418521633d56
Katsuhisa Kitano <91katsuhisa@gmail.com>
2020-07-12 16:11:45 +0900

    書籍にあわせてプログラムを削除

--- openpyxl/set_excel_layout.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-
-wb = openpyxl.Workbook()
-ws = wb.active
-ws.row_dimensions[1].height = 100
-# 1行目を非表示にする
-# ws.row_dimensions[1].hidden = True
-ws.column_dimensions['B'].width = 50
-wb.save('test.xlsx')
