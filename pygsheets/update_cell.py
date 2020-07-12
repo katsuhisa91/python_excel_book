5053837820c78feda89e664f3f0ab06564b05b6f
Katsuhisa Kitano <91katsuhisa@gmail.com>
2020-07-12 16:42:08 +0900

    書籍にあわせてプログラムを更新する

--- pygsheets/update_cell.py
+++ pygsheets/update_cell.py
@@ -4,8 +4,4 @@ gc = pygsheets.authorize(service_file='pygsheets-xxxxxxxxxxxx.json')
 sp = gc.open_by_key('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 wks = sp.worksheet('index', 0)
-# wks.update_value((1, 3), '10000') でもOK
-wks.update_value('C1', '100')
-
-sp.share('example@gmail.com', role='reader')
-print('https://docs.google.com/spreadsheets/d/' + sp.id)
+wks.update_value('C1', 150)
